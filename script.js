8a5925c9d2c2ab7888357860e7ae45df87eb5237
t-hanafin <thanafin@outlook.com>
2023-02-14 15:08:47 -0500

    add a button that sets the grid size according to user input

--- script.js
+++ script.js
@@ -1,66 +1,77 @@
-let num = parseInt(prompt("Grid size, please"));
+let num;
 let gridSize;
 
-switch(num) {
-    case 0:
-        break;
-    case 1:
-        gridSize = 100;
-        break;
-    case 2:
-        gridSize = 49;
-        break;
-    case 3:
-        gridSize = 33;
-        break;
-    case 4:
-        gridSize = 24;
-        break;
-    case 5:
-        gridSize = 19;
-        break;
-    case 6:
-        gridSize = 16;
-        break;
-    case 7:
-        gridSize = 13.5;
-        break;
-    case 8:
-        gridSize = 12;
-        break;
-    case 9:
-        gridSize = 11;
-        break;
-    case 10:
-        gridSize = 9;
-        break;
-    case 11:
-        gridSize = 8.5;
-        break;
-    case 12:
-        gridSize = 7.5;
-        break;
-    case 13:
-        gridSize = 7;
-        break;
-    case 14:
-        gridSize = 6.5;
-        break;
-    case 15:
-        gridSize = 6;
-        break;
-    case 16:
-        gridSize = 5.5;
-        break;
-    case 17:
-        gridSize = 5.3;
-        break;
-    case 18:
-        gridSize = 5;
-        break;
-    default:
-        gridSize = 12;
+function getSize() {
+    gridContainer.innerHTML = "";
+    num = parseInt(prompt("Grid size, please"));
+    getGridSize(num);
+    gridGenerator(num);
 }
+
+function getGridSize(num) {
+    switch(num) {
+        case 0:
+            break;
+        case 1:
+            gridSize = 100;
+            break;
+        case 2:
+            gridSize = 49;
+            break;
+        case 3:
+            gridSize = 33;
+            break;
+        case 4:
+            gridSize = 24;
+            break;
+        case 5:
+            gridSize = 19;
+            break;
+        case 6:
+            gridSize = 16;
+            break;
+        case 7:
+            gridSize = 13.5;
+            break;
+        case 8:
+            gridSize = 12;
+            break;
+        case 9:
+            gridSize = 10.8;
+            break;
+        case 10:
+            gridSize = 9;
+            break;
+        case 11:
+            gridSize = 8.5;
+            break;
+        case 12:
+            gridSize = 7.5;
+            break;
+        case 13:
+            gridSize = 7;
+            break;
+        case 14:
+            gridSize = 6.5;
+            break;
+        case 15:
+            gridSize = 5.8;
+            break;
+        case 16:
+            gridSize = 5.5;
+            break;
+        case 17:
+            gridSize = 5.3;
+            break;
+        case 18:
+            gridSize = 5.3;
+            break;
+        default:
+            gridSize = 12;
+    }
+}
+
+
 
 const gridContainer = document.querySelector('.grid-container');
 
@@ -79,9 +90,7 @@ function gridGenerator(num) {
         gridSquare.addEventListener("mouseout", event => {
             console.log("Mouse out");
         })
-
     }
 }
 
-gridGenerator(num);
 
